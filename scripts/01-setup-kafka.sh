de5574619451583dbf91d964e4949d94c618c987
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-02-24 08:11:05 +0000

    Correct bootstrap server local dns.
    Add debug output for connector calls

--- scripts/01-setup-kafka.sh
+++ scripts/01-setup-kafka.sh
@@ -54,6 +54,14 @@ oc start-build my-connect-cluster-connect --from-dir=$DEMO_HOME/kube/kafka/conne
 # expose the connect API service
 oc expose svc my-connect-cluster-connect-api
 
+# record the route
+DBZ_CONNECTOR=$(oc get route my-connect-cluster-connect-api -o jsonpath='{.spec.host}')
+
+# check the the service is up
+echo "Checking that the connector is online:"
+curl -H "Accept:application/json" $DBZ_CONNECTOR/
+echo "\ndone."
+
 # Configure the connector by calling through to its API
 # The values therein are coming from values set from the previous script and yaml files
 curl -X PUT -H "Content-Type: application/json" \
@@ -67,7 +75,7 @@ curl -X PUT -H "Content-Type: application/json" \
     "database.server.id": "184054",
     "database.server.name": "sampledb", 
     "database.whitelist": "sampledb",
-    "database.history.kafka.bootstrap.servers": "my-cluster-kafka-bootstrap.svc:9092", 
+    "database.history.kafka.bootstrap.servers": "my-cluster-kafka-bootstrap:9092", 
     "database.history.kafka.topic": "changes-topic",
     "decimal.handling.mode" : "double",
     "transforms": "route",
@@ -75,4 +83,8 @@ curl -X PUT -H "Content-Type: application/json" \
     "transforms.route.regex": "([^.]+)\\.([^.]+)\\.([^.]+)",
     "transforms.route.replacement": "$3"
 }' \
-    http://$(oc get route my-connect-cluster-connect-api -o jsonpath='{.spec.host}')/connectors/debezium-connector-mysql/config
+    http://$DBZ_CONNECTOR/connectors/debezium-connector-mysql/config
+
+echo "Checking that the mysql connector has been initialized:"
+curl -H "Accept:application/json" $DBZ_CONNECTOR/connectors
+echo "\ndone."
