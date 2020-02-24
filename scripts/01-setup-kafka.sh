040fa6ecc64b13942e28bbf29fe4d7f7a511addb
Marc Hildenbrand <mwhildenbrand@gmail.com>
2020-02-24 14:37:59 +0000

    additional deployment wait conditions

--- scripts/01-setup-kafka.sh
+++ scripts/01-setup-kafka.sh
@@ -5,7 +5,7 @@ DEMO_HOME=$SCRIPT_DIR/..
 
 
 USER=$1
-PASSWORD=$2
+PASSWORD="$2"
 
 if [ -z "$USER" ]; then
     echo "Must specify a user for registry.redhat.io"
@@ -29,27 +29,32 @@ oc apply -f $DEMO_HOME/kube/setup/subscription.yaml
 oc apply -f $DEMO_HOME/kube/kafka/kafka.yaml
 oc apply -f $DEMO_HOME/kube/kafka/kafka-topic.yaml
 
-# wait until the cluster is deployed
-# FIXME: my-cluster is assumed to be the name of the cluster for the demo
-echo "Waiting up to 6 minutes for kafka cluster to be ready"
-oc wait --for=condition=Ready kafka/my-cluster --timeout=360
-echo "Kafka cluster is ready."
-
 # Create a secret for the registry the kafkaconnects2i depends on
 # redhat.registry.io 
 oc create secret docker-registry connects2i \
     --docker-server=registry.redhat.io \
-    --docker-username=$USER \
-    --docker-password=$PASSWORD \
+    --docker-username="$USER" \
+    --docker-password="$PASSWORD" \
     --docker-email=mhildenb@redhat.com
+
+# wait until the cluster is deployed
+# FIXME: my-cluster is assumed to be the name of the cluster for the demo
+echo "Waiting up to 6 minutes for kafka cluster to be ready"
+oc wait --for=condition=Ready kafka/my-cluster --timeout=360s
+echo "Kafka cluster is ready."
 
 # create an connect S2I cluster
 oc apply -f $DEMO_HOME/kube/kafka/kafkaconnects2i-my-connect-cluster.yaml
 
+# might have to wait to pull the image
+sleep 5
 # build config should appear immediately if it doesn't have trouble pulling the image
 
 # build the connector
 oc start-build my-connect-cluster-connect --from-dir=$DEMO_HOME/kube/kafka/connect-plugins --follow
+
+# wait until the cluster is deployed
+oc wait --for=condition=available dc/my-connect-cluster-connect
 
 # expose the connect API service
 oc expose svc my-connect-cluster-connect-api
--- scripts/02-setup-app.sh
+++ scripts/02-setup-app.sh
@@ -5,6 +5,10 @@
 oc new-app --docker-image=quay.io/quarkus/ubi-quarkus-native-s2i:19.0.2 --name "quarkus-transaction-crud" --binary
 oc patch bc/quarkus-transaction-crud -p '{"spec":{"resources":{"limits":{"cpu":"5", "memory":"6Gi"}}}}'
 oc start-build bc/quarkus-transaction-crud --from-dir=$DEMO_HOME/demo-crud-app --follow
+
+# wait for the application to actually be deployed
+oc wait --for=condition=available dc/quarkus-transaction-crud
+
 oc expose svc/quarkus-transaction-crud
 
 export TRANS_APP=$(oc get route quarkus-transaction-crud -n debezium-cdc -o jsonpath='{.spec.host}')
