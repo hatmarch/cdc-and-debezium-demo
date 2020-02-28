cb3e95710008adc27ea2b72bd8567b570339710a
Marc Hildenbrand <mwhildenbrand@gmail.com>
2020-02-28 14:10:26 +0000

    Add directions and images for initial app setup
    00 script now creates the app and database at the same time
    
    Explicitly adds the current oc user to anyuid scc
    
    Add longer timeout for quarkus build

--- scripts/00-setup-app.sh
+++ scripts/00-setup-app.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # NOTE: account that runs this script needs the run as root priviledge (scc anyuid)
+oc adm policy add-scc-to-user anyuid $(oc whoami)
 
 # Create a new project
 oc new-project debezium-cdc
+
+# Create a database (that is setup with necessary binlog)
 oc new-app --name=mysql debezium/example-mysql:1.1 \
                         -e MYSQL_ROOT_PASSWORD=password \
                         -e MYSQL_USER=testUser \
@@ -29,3 +32,18 @@ oc exec -n debezium-cdc -i "${MYSQL_POD}" -- bash -c 'mysql -u root -ppassword -
 oc exec  -n debezium-cdc -i "${MYSQL_POD}" -- bash -c 'mysql -A -u root -ppassword -h mysql sampledb -e "select count(*) from transaction;"'
 
 echo "done"
+
+
+# create a S2I build but set as binary so that we can upload our local directory instead of having to checkin
+# (this is what the --binary flag does)
+oc new-app --docker-image=quay.io/quarkus/ubi-quarkus-native-s2i:19.0.2 --name "quarkus-transaction-crud" --binary
+oc patch bc/quarkus-transaction-crud -p '{"spec":{"resources":{"limits":{"cpu":"5", "memory":"6Gi"}}}}'
+oc start-build bc/quarkus-transaction-crud --from-dir=$DEMO_HOME/demo-crud-app --follow
+
+# wait for the application to actually be deployed
+oc wait --for=condition=available dc/quarkus-transaction-crud --timeout=20m
+
+oc expose svc/quarkus-transaction-crud
+
+export TRANS_APP=$(oc get route quarkus-transaction-crud -n debezium-cdc -o jsonpath='{.spec.host}')
+echo "Transaction generating application can be found at http://${TRANS_APP}/"
