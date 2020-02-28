67270971d039dfd98fadb592a04e616a5b1409cd
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-02-28 05:10:46 +0000

    Custom monitoring script updated for OpenShift 4.3+

--- scripts/03-setup-custom-monitoring.sh
+++ scripts/03-setup-custom-monitoring.sh
@@ -3,6 +3,17 @@
 # This file follows the directions that are outlined here: 
 # https://www.redhat.com/en/blog/custom-grafana-dashboards-red-hat-openshift-container-platform-4
 
+# Setup central monitoring
+oc apply -f $DEMO_HOME/kube/grafana/cluster-monitoring-config.yaml
+
+# wait for user workload pods to come up in new project
+sleep 5
+oc -n openshift-user-workload-monitoring wait --for=condition=available po/prometheus-user-workload-0
+
+# deploy service monitor to scrape metrics into central prometheus monitoring
+oc apply -f /workspaces/cdc-and-debezium-demo/kube/grafana/service-monitor-debezium-connector.yaml -n debezium-cdc
+
+
 if [ -z "$1" ]; then
     PROJECT_NAME="debezium-monitoring"
 else
@@ -26,14 +37,17 @@ oc wait --for=condition=available deployment/grafana-operator
 oc process -f $DEMO_HOME/kube/grafana/grafana-instance-template.yaml PROJECT_NAME="${PROJECT_NAME}" \
     ADMIN_PASSWORD="${ADMIN_PASSWORD}" -o yaml | oc apply -f -
 
+# wait a few minutes for the deployment to appear
+sleep 5
+
 # Wait until grafana instance is deployed
 oc wait --for=condition=available deployment/grafana-deployment
 
 # Patch Prometheus so that it can connect with the customer grafana instance in our project
 # FIXME: doing by index of array seems dangerous, but it doesn't appear that there is another way to do this with patching
-# specifications
-oc patch statefulsets/prometheus-k8s -n openshift-monitoring --type='json' \
-    -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/args/9","value": "--web.listen-address=:9090" }]'
+# specifications..besides, this no longer works in 4.3
+# oc patch statefulsets/prometheus-k8s -n openshift-monitoring --type='json' \
+#     -p='[ { "op": "replace", "path": "/spec/template/spec/containers/0/args/9","value": "--web.listen-address=:9090" }]'
 
 # PRint out the route
 echo "https://$(oc get route grafana-route -o jsonpath='{.spec.host}' -n ${PROJECT_NAME})/"
