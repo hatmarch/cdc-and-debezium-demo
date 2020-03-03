c621c86b792b66c712e14cad9bf621f58efef980
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-03-03 05:45:44 +0000

    Check conditions properly and add operator group
    Adds operator group as the new way to install operators from CLI
    
    Fixes a bunch of bugs in the fuse setup script (waits for operator deployment, checks for secret correctly, does proper numerical checks for count)

--- scripts/06-fuse-setup.sh
+++ scripts/06-fuse-setup.sh
@@ -1,37 +1,45 @@
 #!/bin/bash
 
-oc project debezium-cdc
+PROJECT_NAME=debezium-cdc
 
-# install Fuse operator and subscription
-oc apply -f $DEMO_HOME/kube/fuse/fuse-operator-subscription.yaml
-# oc apply -f $DEMO_HOME/fuse/fuse-csv-7.5.0.yaml
+oc project $PROJECT_NAME
 
 # NOTE: This uses the same docker-registry secret as created in 01-setup-kafka.sh
-if [ -z $(oc get secret connects2i --no-headers=true 2>/dev/null) ]; then
+if [ -z "$(oc get secret connects2i --no-headers=true 2> /dev/null)" ]; then
     echo "Need to create pull-secret for registry.io"
     exit 1
 fi
 
-# link the operator to the secret
+# install Fuse operator and subscription
+oc apply -f $DEMO_HOME/kube/fuse/fuse-operator-subscription.yaml
+
+# install operator group
+oc apply -f $DEMO_HOME/kube/fuse/fuse-operator-group.yaml
+
+# link the operator to the secret above
 oc secrets link syndesis-operator connects2i --for=pull
+
+# wait for syndesis operator deployment
+sleep 5
+oc wait --for=condition=available deployment/syndesis-operator --timeout=5m
 
 # create a fuse online instance
 oc apply -f $DEMO_HOME/kube/fuse/syndesis.yaml
 
 # state check on Syndesis CR is based on documentation of states here: 
 # https://github.com/syndesisio/syndesis/tree/master/install/operator#available-fields
-while true do
-    CURRENT_STATUS=$(oc get syndesis app -o jsonpath='{.status.phase}' -n debezium-cdc)
-    if [ $CURRENT_STATUS eq "Installed" ]; then
+while true; do
+    CURRENT_STATUS=$(oc get syndesis app -o jsonpath='{.status.phase}' -n $PROJECT_NAME)
+    if [ "$CURRENT_STATUS" = "Installed" ]; then
         break
     fi
 
-    if [ $CURRENT_STATUS eq "StartupFailed" ]; then
+    if [ "$CURRENT_STATUS" = "StartupFailed" ]; then
         "Failed to install FuseOnline.  Try reinstalling the Syndesis CR"
         exit 1
     fi
 
-    if [ $CURRENT_STATUS eq "UpgradeFailed" ]; then
+    if [ "$CURRENT_STATUS" = "UpgradeFailed" ]; then
         "Failed to install or upgrade FuseOnline.  Try reinstalling the Syndesis CR"
         exit 1
     fi
@@ -42,4 +50,4 @@ done
 echo "Finished installing FuseOnline instance"
 
 echo "Find the fuse route here:"
-echo "http://$(oc get route syndesis -o jsonpath='{.spec.host}')"
+echo "http://$(oc get route syndesis -o jsonpath='{.spec.host}' -n $PROJECT_NAME)"
