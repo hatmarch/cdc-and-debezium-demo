493f595c6b6370d587ca0e59d1d28efb3338ebaf
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-03-06 08:40:13 +0000

    Initial changes to set groundwork.  WIP

---
+++ scripts/05-optional-setup-es-proxy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Find the token secret for default account
+
+# Make that default account cluster admin  (FIXME)
+
+# create a configmap from virtserv.conf
+
+# process the template
+
+# Add a service to map to the template (port 8080)
